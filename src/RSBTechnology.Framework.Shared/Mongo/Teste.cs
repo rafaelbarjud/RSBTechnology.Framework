423c0717f49cbdbfeaf128b4dce6bb86f7f9bbbf
Rafael de Souza Barjud <rafaelbarjud@gmail.com>
2025-01-30 18:44:29 -0300

    Teste

---
+++ src/RSBTechnology.Framework.Shared/Mongo/Teste.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSBTechnology.Framework.Shared.Mongo
+{
+    internal static class Teste
+    {
+        internal static int TesteDez()
+            => 10 + 10;
+    }
+}
